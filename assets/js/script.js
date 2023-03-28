ae674e6150eb8fdadabc0552e511426d6bebb8b6
alizadaln <lalana@code.edu.az>
2023-03-28 21:52:17 +0400

    container cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
 
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
+// function myFunction() {
+//     var x = document.getElementById("myTopnav");
+//     if (x.className === "topnav") {
+//       x.className += " responsive";
+//     } else {
+//       x.className = "topnav";
+//     }
+//   }
 
-  var myModal = document.getElementById('myModal')
-  var myInput = document.getElementById('myInput')
+//   var myModal = document.getElementById('myModal')
+//   var myInput = document.getElementById('myInput')
 
-  myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
-  })
+  // myModal.addEventListener('shown.bs.modal', function () {
+  // myInput.focus()
+  // })
