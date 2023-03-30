1189be7af2932e6b122feb86501a80689f4a1787
alizadaln <lalana@code.edu.az>
2023-03-31 00:46:31 +0400

    all min cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,1 @@
 
-// function myFunction() {
-//     var x = document.getElementById("myTopnav");
-//     if (x.className === "topnav") {
-//       x.className += " responsive";
-//     } else {
-//       x.className = "topnav";
-//     }
-//   }
-
-//   var myModal = document.getElementById('myModal')
-//   var myInput = document.getElementById('myInput')
-
-  // myModal.addEventListener('shown.bs.modal', function () {
-  // myInput.focus()
-  // })
