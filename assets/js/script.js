9c74df346948850af5939d9b6a5a80aa18fbacef
alizadaln <lalana@code.edu.az>
2023-04-01 19:55:32 +0400

    continue HomePage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,78 @@
+$(document).ready(function () {
+
+    $('.productsSlide').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+});
+
+
+
+
+const endDate = new Date('2023-04-07T23:59:59').getTime();
+
+
+const countdown = setInterval(() => {
+
+    const now = new Date().getTime();
+
+
+    const distance = endDate - now;
+
+    
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2, '0');
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000).toString().padStart(2, '0');
+
+
+    document.getElementById('countdown').innerHTML = `${days} : ${hours} : ${minutes} : ${seconds}`;
+    document.getElementById('days').innerHTML = pad(days);
+    document.getElementById('hours').innerHTML = pad(hours);
+    document.getElementById('minutes').innerHTML = pad(minutes);
+    document.getElementById('seconds').innerHTML = pad(seconds);
+
+
+    if (distance < 0) {
+        clearInterval(countdown);
+        document.getElementById('countdown').innerHTML = 'EXPIRED';
+    }
+}, 1000);
+
+function pad(number) {
+    return number.toString().padStart(2, '0');
+}
+
+
 
