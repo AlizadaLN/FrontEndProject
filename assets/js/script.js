318bd0f92bb3ee4f1ff01636d4b4da1141888c3d
alizadaln <lalana@code.edu.az>
2023-04-02 16:45:16 +0400

    Login Register

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,4 +75,20 @@ function pad(number) {
 }
 
 
+// LogIn Register
+
+let loginLink = document.querySelector('.login-link');
+let registerLink = document.querySelector('.register-link');
+let loginForm = document.querySelector('.login-form');
+let registerForm = document.querySelector('.register-form');
+
+loginLink.addEventListener('click', () => {
+    loginForm.style.display = 'block';
+    registerForm.style.display = 'none';
+});
+
+registerLink.addEventListener('click', () => {
+    loginForm.style.display = 'none';
+    registerForm.style.display = 'block';
+});
 
